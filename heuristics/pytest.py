4f15cbd76a732e46cf93e7c9d49f1531aa2d062b
Lívia Almeida <liviaab7@gmail.com>
2021-01-25 20:11:38 -0300

    fix regex

--- heuristics/pytest.py
+++ heuristics/pytest.py
@@ -1,2 +1,3 @@
 PYTEST_REGEX = "import\s*pytest"
-PYTEST_REGEX += "@pytest" # @pytest.fixture, @pytest.mark.asyncio
+PYTEST_REGEX += "|@pytest" # @pytest.fixture, @pytest.mark.asyncio
+PYTEST_REGEX += "|pytest"

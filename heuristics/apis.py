9d7cb1ace61a5e82c711ede0656a4a67e71efcba
Lívia Almeida <liviaab7@gmail.com>
2021-06-04 10:06:21 -0300

    Improve accuracy in some api regex

--- heuristics/apis.py
+++ heuristics/apis.py
@@ -4,13 +4,13 @@ def count_pattern_in_content(pattern, content):
 	return len(re.findall(pattern, content))
 
 class UnittestAPIHeuristics:
-	testCaseSubclass_pattern = "\(.*TestCase\)"
-	assert_pattern = "self.assert.*\("
-	setUp_pattern = "def\s+setUp\("
-	setUpClass_pattern = "def\s+setUpClass\("
-	tearDown_pattern = "def\s+tearDown\("
-	tearDownClass_pattern = "def\s+tearDownClass\("
-	skipTest_pattern = "@unittest.skip.*\(|self.skipTest"
+	testCaseSubclass_pattern = "\(.*TestCase\):"
+	assert_pattern = "self.assert.*\(.*\)"
+	setUp_pattern = "def\s+setUp\(.*\):"
+	setUpClass_pattern = "def\s+setUpClass\(.*\):"
+	tearDown_pattern = "def\s+tearDown\(.*\):"
+	tearDownClass_pattern = "def\s+tearDownClass\(.*\):"
+	skipTest_pattern = "[@]{0,1}unittest.skip.*?\((.*\))|self.skipTest(\(.*\))"
 	expectedFailure_pattern = "@unittest.expectedFailure"
 
 	@classmethod
@@ -31,10 +31,15 @@ class UnittestAPIHeuristics:
 
 class PytestAPIHeuristics:
 	native_assert_pattern = "\s*assert\s+"
-	raiseError_pattern = "\s*raise\s+|pytest.raises\("
-	skipTest_pattern = "pytest.skip\(|@pytest.mark.skip\(|@pytest.mark.skipIf\("
-	expectedFailure_pattern = "@pytest.mark.xfail|pytest.xfail\("
-	fixture_pattern = "@pytest.mark.usefixtures\(|@pytest.fixture"
+	raise_pattern = "\s*raise\s+"
+	pytestRaise_pattern = "pytest.raises(\(.*\))"
+	simpleSkip_pattern = "pytest.skip(\(.*\))"
+	markSkip_pattern = "[@]{0,1}pytest.mark.skip.*?(\(.*\))"
+	expectedFailure_pattern = "[@]{0,1}pytest.mark.xfail(\(.*\))|[@]{0,1}pytest.mark.xfail"s
+	fixture_pattern = "@pytest.fixture(.*)" # add generic pattern to @pytest.fixture
+	usefixture_pattern = "@pytest.mark.usefixtures(\(.*\))"
+	generalMark_pattern = "@{0,1}pytest.mark\.(.*?\()(.*\))" # add generic pattern to @pytest.mark
+	generalPytest_pattern = "@pytest\.(.*?\()(.*\))" # add generic pattern to @pytest.mark
 
 	@classmethod
 	def count_apis(cls, content):

6e4c53896bc3f5a1c9436a29161fd524e2bf710e
liviaab <liviaab7@gmail.com>
2021-07-23 08:44:54 -0300

    fix unittest and pytest heuristics

--- heuristics/pytest.py
+++ heuristics/pytest.py
@@ -1,4 +1,5 @@
-from pyparsing import Regex, pythonStyleComment, quotedString
+import re
+from pyparsing import Regex, pythonStyleComment, dblQuotedString, sglQuotedString, quotedString
 from common.common import docString
 
 class PytestHeuristics:
@@ -6,14 +7,22 @@ class PytestHeuristics:
 
     @classmethod
     def matches_a(cls, text, ignoreComments=True):
-        if text == None:
+        if text == None or text.strip() == '':
             return False
 
-        expr = Regex(cls.regex).ignore(quotedString | docString)
+        expr = Regex(r'.*').ignore(quotedString | dblQuotedString | sglQuotedString | docString)
         if ignoreComments:
-            expr = Regex(cls.regex).ignore(pythonStyleComment | quotedString | docString)
+            expr = Regex(r'.*').ignore(pythonStyleComment | quotedString | dblQuotedString | sglQuotedString | docString)
 
-        return list(expr.scanString(text)) != []
+        filtered_result = list(expr.scanString(text))
+        filtered_result = [ re.sub("[\"|\'](.*)[\"|\']", "", text[0]) for (text, _, _ ) in filtered_result]
+        matches = []
+        for result in filtered_result:
+            match = re.findall(cls.regex, result)
+            if match != []:
+                matches.append(match[0])
+
+        return matches != []
 
     @classmethod
     def matches_any(cls, text_list, ignoreComments=True):
